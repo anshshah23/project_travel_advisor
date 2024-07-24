6ad232bb30f4003f34e599a11b0ec0f26cd44455
anshshah23 <ANSH.SHAH082@svkmmumbai.onmicrosoft.com>
2024-07-25 00:32:34 +0530

    Updated with cards

--- src/App.js
+++ src/App.js
@@ -12,12 +12,21 @@ const App = () => {
   const [coordinates, setCoordinates] = useState({});
   const [bounds, setBounds] = useState(null);
   const [type, setType] = useState('restaurants');
+  const [rating, setRating] = useState('3');
+  const [filteredPlaces, setFilteredPlaces] = useState([]);
+  const [childClicked, setChildClicked] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(({ coords: { latitude, longitude } }) => {
       setCoordinates({ lat: latitude, lng: longitude });
     });
   }, []);
+
+  useEffect(() => {
+    const filteredPlaces = places.filter((place) => place.rating>= rating);
+    setFilteredPlaces(filteredPlaces);
+  }, [places, rating]);
 
   useEffect(() => {
     if (bounds) {
@@ -35,13 +44,19 @@ const App = () => {
       <Header />
       <Grid container spacing={3} style={{ width: '100%' }}>
         <Grid item xs={12} md={4}>
-          <List places={places} type={type} setType={setType} />
+          <List
+            isLoading={isLoading}
+            childClicked={childClicked}
+            places = {filteredPlaces.length ? filteredPlaces : places}
+             type={type} setType={setType} rating={rating} setRating={setRating} />
         </Grid>
         <Grid item xs={12} md={8} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
           <Map
             setCoordinates={setCoordinates}
             setBounds={setBounds}
             coordinates={coordinates}
+            places={filteredPlaces.length ? filteredPlaces : places}
+            setChildClicked={setChildClicked}
           />
         </Grid>
       </Grid>
--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -4,9 +4,8 @@ import PlaceDetails from '../PlaceDetails/PlaceDetails';
 
 import useStyles from './styles';
 
-const List = ({ places, type, setType }) => {
+const List = ({ places, type, setType, rating, setRating }) => {
   const classes = useStyles();
-  const [rating, setRating] = React.useState('3');
 
   return (
     <div className={classes.container}>
@@ -23,13 +22,12 @@ const List = ({ places, type, setType }) => {
         <InputLabel>Rating</InputLabel>
         <Select value={rating} onChange={(e) => setRating(e.target.value)}>
           <MenuItem value={0}>All</MenuItem>
-          <MenuItem value={2}>Above 2.0</MenuItem>
           <MenuItem value={3}>Above 3.0</MenuItem>
           <MenuItem value={4}>Above 4.0</MenuItem>
           <MenuItem value={4.5}>Above 4.5</MenuItem>
         </Select>
       </FormControl>
-      <Grid container spacing={3} className={classes.list}>
+      <Grid container spacing={3} className={classes.list} style={{marginTop:'5px'}}>
         {places?.map((place, i) => (
           <Grid item key={i} xs={12}>
             <PlaceDetails place={place} />
