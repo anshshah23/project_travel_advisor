f3cf65e9ff54ddd0906a5d530c151c15ab07ac50
anshshah23 <ANSH.SHAH082@svkmmumbai.onmicrosoft.com>
2024-07-24 15:21:12 +0530

    Errors

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,29 +1,9 @@
-import React from 'react';
-import GoogleMapReact from 'google-map-react';
-import { Paper, Typography, useMediaQuery } from '@mui/material';
-import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
-import Rating from '@mui/lab/Rating';
+import React from 'react'
 
-import mapStyles from '../../mapStyles';
-import useStyles from './styles.js';
-
-const Map = ({ coords, places, setCoords, setBounds, setChildClicked, weatherData }) => {
-  const matches = useMediaQuery('(min-width:600px)');
-  const classes = useStyles();
-
+const Map = () => {
   return (
-    <div className={classes.mapContainer}>
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: 'AIzaSyCHor5aL_GCg7p6JkcHZMHU-0hMkpwOtWA' }}
-        defaultCenter={coords}
-        center={coords}
-        defaultZoom={14}
-        margin={[50, 50, 50, 50]}
-        options={{ disableDefaultUI: true, zoomControl: true, styles: mapStyles }}
-      >
-      </GoogleMapReact>
-    </div>
-  );
-};
+    <div>Map</div>
+  )
+}
 
-export default Map;
+export default Map
